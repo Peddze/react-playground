ef106472aaeb0b7b9d3a993b622d505b652099a7
Peddze <peddze@proton.me>
2026-06-05 17:57:43 +0100

    feat: add Counter component with useState

---
+++ src/Counter.jsx
@@ -0,0 +1,21 @@
+import{ useState } from 'react'
+
+function Counter() {
+    const [count, setCount] = useState(0)
+    const handleClick = () => {
+        setCount(count + 1);
+    };
+    const handleReset = () => {
+        setCount(0);
+    };
+
+    return (
+        <div>
+            <h4>The current count is {count}</h4>
+            <button type='button' className='counter' onClick={handleClick}>Increment</button>
+            <button type='button' className='counter' onClick={handleReset}>Reset</button>
+        </div>
+    );
+}
+
+export default Counter
